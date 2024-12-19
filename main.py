c807220127b5e0779fec50461838114fa6ebb8f3
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-20 04:15:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ def login_screen():
     print(Fore.CYAN + "="*40)
     print(Fore.MAGENTA + Style.BRIGHT + "         ระบบล็อคอิน")
     print(Fore.CYAN + "="*40)
-    print(Fore.WHITE + "กรุณากรอกข้อมูลเพื่อเข้าสู่ระบบ\n")
+    print(Fore.WHITE + "ติดต่อแอดมินเพื่อสมัครสมาชิก/n   https://www.facebook.com/earthkcc147?mibextid=ZbWKwL")
+    print(Fore.WHITE + "กรุณากรอกข้อมูลเพื่อเข้าสู่ระบบ")
+    
 
 # แสดงหน้าล็อคอิน
 login_screen()
