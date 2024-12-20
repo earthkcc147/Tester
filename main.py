bdeacf6f36130eac262188324717b29463175c34
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-21 06:44:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -58,6 +58,10 @@ def print_welcome_message(username):
         f"🔧 CPU: {device_info['Device']['processor']} ({device_info['Device']['cpu_count']} cores)\n"
         f"🔋 แบตเตอรี่: {device_info['Battery']}\n"
         f"🖥️ ความละเอียดหน้าจอ: {device_info['Screen Resolution']}\n"
+
+        f"💾 RAM: {device_info['Memory']['total']} (Used: {device_info['Memory']['used']}, Free: {device_info['Memory']['free']}, Usage: {device_info['Memory']['percent']})\n"
+        f"🌐 เครือข่าย: {device_info['Network']}\n"
+        f"🔓 พอร์ตที่เปิด: {device_info['Open Ports']}\n"
         
         "🔔 ยินดีต้อนรับเข้าสู่ระบบ!"
     )
